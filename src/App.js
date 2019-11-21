5ca597088a283ea47fa56332e4c5a9fa6367ffa6
Jacob Cacciamani <jacob@Jacobs-MacBook-Pro-2.local>
2019-11-21 15:17:41 -0500

    Added images, changed colors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import NavBox from './components/NavBox/NavBox';
 import Home from './components/Sections/Home';
 import About from './components/Sections/About';
 import Projects from './components/Sections/Projects';
-import Contact from './components/Sections/Contact';
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +14,6 @@ export default class App extends React.Component {
         <Home />
         <About />
         <Projects />
-        <Contact />
       </div>
     );
   }
--- src/components/NavBox/NavBox.js
+++ src/components/NavBox/NavBox.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-scroll";
+import GitHub from "../../images/GitHub-Mark-Light-32px.png";
+import LinkedIn from "../../images/LI-In-Bug.png";
 
 export default class NavBox extends Component {
     state = {};
@@ -7,46 +9,46 @@ export default class NavBox extends Component {
     render() {
         return (
             <div className="navBox">
-                <Link
-                    activeClass="active"
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration= {400}
-                > 
-                    Home 
-                </Link>
-                <Link
-                    activeClass="active"
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration= {400}
-                > 
-                    About 
-                </Link>
-                <Link
-                    activeClass="active"
-                    to="projects"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration= {400}
-                > 
-                    Projects 
-                </Link>
-                <Link
-                    activeClass="active"
-                    to="contact"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration= {400}
-                > 
-                    Contact 
-                </Link>
+                <div className="scrollLinks">
+                    <Link
+                        activeClass="active"
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration= {400}
+                    > 
+                        Home 
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration= {400}
+                    > 
+                        About 
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration= {400}
+                    > 
+                        Projects 
+                    </Link>
+                </div>
+                <div className="outLinks">
+                    <a href="https://github.com/cacciamani10" target="_blank" rel="noopener noreferrer">
+                        <img className="outLinkPicture" src={GitHub} alt="Github"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/jacob-cacciamani-727471176/" target="_blank" rel="noopener noreferrer">
+                        <img className="outLinkPicture" src={LinkedIn} alt="LinkedIn"/>
+                    </a>
+                </div>
             </div>
         )
     }
