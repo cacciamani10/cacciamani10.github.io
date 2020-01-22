c78b0c3de16c18b26854f5443df578f794041545
Jacob Cacciamani <jacob@r48-19-171-108.wccnet.edu>
2020-01-21 20:35:58 -0500

    Trying to fix background image not appearing, adding cname

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "cacciamani10.github.io/precache-manifest.28723e2cb342a7d55fc202e4661814b1.js"
+  "cacciamani10.github.io/precache-manifest.1218391718792ae148e04a57234e1394.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Sections/Home.js
+++ src/components/Sections/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import BackgroundImage from '../../images/treesandstars2.jpg';
+
+var backgroundStyle = {
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover'
+};
 
 export default function Home() {
     return (
-        <div className="section home">
+        <div className="section" style={backgroundStyle}>
             <div className="heading">
                 <h1 className="name">Jacob Cacciamani</h1>
                 <h3 className="title">Full Stack Web Developer</h3>
